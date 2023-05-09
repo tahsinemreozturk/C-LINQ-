2203d7996426bbaa8e6277164cb5db2a5bcabff3
Bestf <Bestf@DESKTOP-5UL5PTI>
2023-05-09 18:08:38 +0200

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProdcutDal _productDal;
+
+        public ProductManager(IProdcutDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //is kodlari
+            return _productDal.GetAll();    
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+        foreach (var prodcut in productManager.GetAll())
+        {
+            Console.WriteLine(prodcut.ProductName);
+        }
+
+        
+    }
+}
